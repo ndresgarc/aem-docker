07305520806204bcba9e95c5d6552787f120126d
ndresgarc <email@andresgarcia.dev>
2023-12-17 22:36:40 +0100

    feat: create init.sh

---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+java -jar /aem/author/aem-author-p4502.jar -nointeractive -Dadmin.password.file=/aem/passwordfile.properties &
+java -jar /aem/publish/aem-publish-p4503.jar -nointeractive -Dadmin.password.file=/aem/passwordfile.properties &
